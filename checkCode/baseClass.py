0b6a94d2297ec25be463757785ba9bc4c11f62bd
liuyongjiang <yongjiang.lyj@alibaba-inc.com>
2018-11-21 21:19:48 +0800

    提交baseclass部分信息

--- checkCode/baseClass.py
+++ checkCode/baseClass.py
@@ -1,4 +1,13 @@
 # -*-  coding:utf-8 -*-
 
-class condition(object):
-    """积累"""
+class commonBase(object):
+    """几个通用的属性，做成通用基类，便于其他父类继承"""
+
+    # count代表通关次数，lv代表玩家等级，career代表玩家职业，gender 代表玩家性别
+    def __init__(self, count, lv, career, gender):
+        self.count = count
+        self.lv = lv
+        self.career = career
+        self.gender = gender
+
+
